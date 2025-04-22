a93dab8dc562f7bfd7588d5b50c7ec8e43b6466b
Logan Bryant <64882175+rhit-bryantlj@users.noreply.github.com>
2025-04-22 12:21:36 -0400

    Update server.cpp for Persistent Connections Extensions

--- CPP/server.cpp
+++ CPP/server.cpp
@@ -14,6 +14,7 @@
 #include <fstream>
 #include <algorithm>
 #include <zlib.h>
+#include <thread>
 
 #define MAX_RECV_LEN 8000
 
@@ -47,7 +48,120 @@ std::string makeResponse(bool encode, std::string encodeType, std::string conten
   }else{
     response = std::format("HTTP/1.1 200 OK\r\nContent-Type: {}\r\nContent-Length: {}\r\n\r\n{}",contentType, size, content);
   }
+
   return response;
+}
+
+void handle_client(int client_fd, std::string dir){
+  char buf[MAX_RECV_LEN];
+  int bytesReceived = 0;
+  bool persist = true;
+  std::string response;
+
+  while(1){
+    bytesReceived = recv(client_fd, buf, sizeof(buf), 0);
+    if(bytesReceived == 0){
+      // should never get here with the test for now but later will maybe come up
+      std::cout << "Nothing received\n";
+      break;
+    }
+    std::string input(buf);
+    int space_pos = input.find(" ");
+    std::string url = "";
+    for(int i = space_pos + 1; i < bytesReceived; i++){
+      if(buf[i] == ' ')
+        break;
+      url += buf[i];
+    }
+    size_t encodingPos = input.find("Accept-Encoding");
+    std::vector<std::string> inputEncodings = {};
+    if(encodingPos != std::string::npos){
+      std::string curEncoding = "";
+      for(int i = (int)encodingPos + 17; i < input.length(); i++){
+        if(input[i] == '\r'){
+          inputEncodings.push_back(curEncoding);
+          break;
+        }
+        if(input[i] == ','){
+          inputEncodings.push_back(curEncoding);
+          curEncoding = "";
+          i++;
+        }else {
+          curEncoding+=input[i];
+        }
+      }
+    }
+    bool encodeResponse = false;
+    std::string encodingType = "";
+    for(int i = 0; i < inputEncodings.size(); i++){
+      if(std::find(encodings.begin(), encodings.end(), inputEncodings[i]) != encodings.end()){ // just exit on the first working encoding for now
+        encodeResponse = true;
+        encodingType = inputEncodings[i];
+        break;
+      }
+    }
+    // test for connection close header
+    if(input.find("Connection: close")!= std::string::npos){
+      persist = false;
+    }
+      
+    if(url == "/"){
+      response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
+    }else if(url.substr(0,6) == "/files"){
+      std::string filepath = dir + url.substr(7);
+      std::ifstream file(filepath);
+      std::cout << "filepath: " + filepath << " type: " + input.substr(0,4) << "\n";
+      if(input.substr(0,4) == "POST"){
+        int pos = input.find("Content-Length:");
+        std::string temp = input.substr(pos+16);
+        pos = temp.find("\r");
+        int content_len = std::stoi(temp.substr(0,pos));
+        std::string content = temp.substr(temp.length()-content_len);
+        std::cout << content << std::endl;
+        if(std::filesystem::exists(dir)){
+          std::ofstream file;
+          file.open(filepath);
+          file.write(content.c_str(), content_len);
+          file.close();
+          response = "HTTP/1.1 201 Created\r\n\r\n";
+        } else {
+          response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
+      } else{
+        if(file.good()){
+          std::stringstream content;
+          content << file.rdbuf();
+          response = makeResponse(encodeResponse, encodingType, "application/octet-stream", content.str().size(), content.str());
+        } else{
+          response = "HTTP/1.1 404 Not Found\r\n\r\n";
+        }
+      }
+    }else if(url.substr(0, 5) == "/echo"){
+      response = makeResponse(encodeResponse, encodingType, "text/plain", url.substr(6).size(), url.substr(6));
+    } else if (url == "/user-agent"){
+      int pos = input.find("User-Agent: ");
+      std::string content = input.substr(pos+12);
+      std::cout << content;
+      response = makeResponse(encodeResponse, encodingType, "text/plain", content.length()-4, content);
+    } else{
+      std::filesystem::path url_path(url);
+      if(std::filesystem::is_directory(url_path)){
+        response = "HTTP/1.1 200 OK\r\n\r\n";
+      } else{
+        response = "HTTP/1.1 404 Not Found\r\n\r\n";
+      }
+    }
+
+    // response message created, either send and close, or just send and receive again
+    if(persist == false){
+      response.insert(response.find("\r\n")+2, "Connection: close\r\n");
+      send(client_fd, response.c_str(), response.length(), 0);
+      break;
+    } else{
+      send(client_fd, response.c_str(), response.length(), 0);
+    }
+  }
+  close(client_fd);
 }
 
 int main(int argc, char **argv) {
@@ -58,7 +172,6 @@ int main(int argc, char **argv) {
   std::string dir;
   if (argc == 3 && strcmp(argv[1], "--directory") == 0)
   	dir = argv[2];
-
 
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
@@ -98,119 +211,10 @@ int main(int argc, char **argv) {
   
     int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
 
-    // Fork for a simple "threaded" application to
-    // TODO: Turn into a threaded example using pthreads
-    pid_t pid = fork();
-    if(pid == 0){  // child
-      std::cout << "Client connected\n";
+    std::cout << "Client connected\n";
 
-      char buf[MAX_RECV_LEN];
-      int bytesReceived = 0;
-
-      bytesReceived = recv(client_fd, buf, sizeof(buf), 0);
-      if(bytesReceived == 0){
-        // should never get here with the test for now but later will maybe come up
-        std::cout << "Nothing received\n";
-        close(client_fd);
-        close(server_fd);
-        return 1;
-      }
-      std::string input(buf);
-      int space_pos = input.find(" ");
-      std::string url = "";
-      for(int i = space_pos + 1; i < bytesReceived; i++){
-        if(buf[i] == ' ')
-          break;
-        url += buf[i];
-      }
-      size_t encodingPos = input.find("Accept-Encoding");
-      std::vector<std::string> inputEncodings = {};
-      if(encodingPos != std::string::npos){
-        std::string curEncoding = "";
-        for(int i = (int)encodingPos + 17; i < input.length(); i++){
-          if(input[i] == '\r'){
-            inputEncodings.push_back(curEncoding);
-            break;
-          }
-          if(input[i] == ','){
-            inputEncodings.push_back(curEncoding);
-            curEncoding = "";
-            i++;
-          }else {
-            curEncoding+=input[i];
-          }
-        }
-      }
-      bool endcodeResponse = false;
-      std::string encodingType = "";
-      for(int i = 0; i < inputEncodings.size(); i++){
-        if(std::find(encodings.begin(), encodings.end(), inputEncodings[i]) != encodings.end()){ // just exit on the first working encoding for now
-          endcodeResponse = true;
-          encodingType = inputEncodings[i];
-          break;
-        }
-      }
-
-      if(url == "/"){
-        const char* response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n";
-        send(client_fd, response, strlen(response), 0);
-      }else if(url.substr(0,6) == "/files"){
-        std::string filepath = dir + url.substr(7);
-        std::ifstream file(filepath);
-        std::cout << "filepath: " + filepath << " type: " + input.substr(0,4) << "\n";
-        if(input.substr(0,4) == "POST"){
-          int pos = input.find("Content-Length:");
-          std::string temp = input.substr(pos+16);
-          pos = temp.find("\r");
-          int content_len = std::stoi(temp.substr(0,pos));
-          std::string content = temp.substr(temp.length()-content_len);
-          std::cout << content << std::endl;
-          if(std::filesystem::exists(dir)){
-            std::ofstream file;
-            file.open(filepath);
-            file.write(content.c_str(), content_len);
-            file.close();
-            const char* response = "HTTP/1.1 201 Created\r\n\r\n";
-            send(client_fd, response, strlen(response), 0);
-          } else {
-            const char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
-            send(client_fd, response, strlen(response), 0);
-          }
-        } else{
-          if(file.good()){
-            std::stringstream content;
-            content << file.rdbuf();
-            std::string file_response = makeResponse(endcodeResponse, encodingType, "application/octet-stream", content.str().size(), content.str());
-            send(client_fd, file_response.c_str(), file_response.length(), 0);
-          } else{
-            const char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
-            send(client_fd, response, strlen(response), 0);
-          }
-        }
-      }else if(url.substr(0, 5) == "/echo"){
-        std::string echo_response = makeResponse(endcodeResponse, encodingType, "text/plain", url.substr(6).size(), url.substr(6));
-        send(client_fd, echo_response.c_str(), echo_response.length(), 0);
-      } else if (url == "/user-agent"){
-        std::cout << "inside user agent" << std::endl;
-        int pos = input.find("User-Agent: ");
-        std::string content = input.substr(pos+12);
-        std::cout << content << "\n";
-        std::string user_response = makeResponse(endcodeResponse, encodingType, "text/plain", content.length()-4, content);
-        send(client_fd, user_response.c_str(), user_response.length(), 0);
-      } else{
-        std::filesystem::path url_path(url);
-        if(std::filesystem::is_directory(url_path)){
-          const char* response = "HTTP/1.1 200 OK\r\n\r\n";
-          send(client_fd, response, strlen(response), 0);
-        } else{
-          const char* response = "HTTP/1.1 404 Not Found\r\n\r\n";
-          send(client_fd, response, strlen(response), 0);
-        }
-      }
-      
-      close(client_fd);
-      return 0;
-    }
+    std::thread t(handle_client, client_fd, dir);
+    t.detach();
   }
   
   close(server_fd);
